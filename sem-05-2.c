5a3bd25cceeb68ecbcca715df063b9f619698357
kseniag03 <114473740+kseniag03@users.noreply.github.com>
2023-02-20 11:52:31 +0300

    Create sem-05-2.c

---
+++ sem-05-2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        printf("No arguments\n");
+        return 0;
+    }
+    
+    int fd;
+    char *fifo = argv[1];
+
+    // open, read, and close the file
+    fd = open(fifo, O_RDONLY);
+    char buffer[100];
+    read(fd, buffer, sizeof(buffer));
+    printf("Received from process 1: %s\n", buffer);
+    close(fd);
+
+    // open, write, and close the file
+    fd = open(fifo, O_WRONLY);
+    char *message = "Hello from process 2!";
+    write(fd, message, strlen(message) + 1);
+    close(fd);
+
+    return 0;
+}

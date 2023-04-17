22cf22cf188b9c368c5e21eb5a9456da11be8bb6
kseniag03 <kaganina@edu.hse.ru>
2023-04-17 03:50:47 +0300

    add-task10

---
+++ 10/1.c
@@ -0,0 +1,55 @@
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+
+int receiver_pid;
+
+void send(int bit) {
+    if (bit == 0) {
+        kill(receiver_pid, SIGUSR1);
+    } else {
+        kill(receiver_pid, SIGUSR2);
+    }
+}
+
+void handler_wait(int nsig) {
+    // waiting...
+}
+
+int main(void) {
+    // установка обработчика сигнала
+    (void) signal(SIGUSR1, handler_wait);
+    
+    int sender_pid = getpid(), num;
+    printf("Sender pid = %d\n\n", sender_pid);
+        
+    printf("Enter receiver pid: ");
+    scanf("%d", &receiver_pid);
+    printf("Enter decimal integer number: ");
+    scanf("%d", &num);
+    
+    printf("\nInput receiver pid: %d\n", receiver_pid);
+    printf("Input decimal integer number: %d\n", num);
+
+    if (num >= 0) {
+        send(0);
+    } else {
+        send(1);
+    }
+
+    int n = num;
+    
+    for (int i = 0; i < 32; ++i) {
+        send(n & 1);
+	n >>= 1;
+        pause();
+    }
+    
+    printf("\nResult = %d\n", num);
+    
+    return 0;
+}
---
+++ 10/2.c
@@ -0,0 +1,52 @@
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+
+int sender_pid;
+int received_bit = -1;
+
+void handler(int nsig) {
+    if (nsig == SIGUSR1) {
+        received_bit = 0;
+    } else if (nsig == SIGUSR1) {
+        received_bit = 1;
+    }
+}
+
+int main(void) {
+    // установка обработчиков сигнала
+    (void) signal(SIGUSR1, handler);
+    (void) signal(SIGUSR2, handler);
+    
+    int receiver_pid = getpid();
+    printf("Receiver pid = %d\n\n", receiver_pid);
+
+    printf("Enter sender pid: ");
+    scanf("%d", &sender_pid);
+
+    printf("\nInput sender pid: %d\n", sender_pid);
+
+    // notify sender
+    kill(sender_pid, SIGUSR1);
+
+    int bits[32];
+    for (int i = 0; i < 32; ++i) {
+        pause();
+	bits[i] = received_bit;
+	printf("%d", received_bit);
+	kill(sender_pid, SIGUSR1);
+    }
+
+    int num = 0;
+    for (int i = 0; i < 32; ++i) {
+        num |= bits[i] << i;
+    }
+
+    printf("\nResult = %d\n", num);
+
+    return 0;
+}
